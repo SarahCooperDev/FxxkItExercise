56976009291be83058a554e4959cd468d07857e0
lmnlty <chaosinelysium@gmail.com>
2023-01-30 05:54:03 +1100

    initial setup with dynamic fragment

---
+++ app/src/main/java/com/example/fxxkit/WorkoutFragment.kt
@@ -0,0 +1,42 @@
+package com.example.fxxkit
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [WorkoutFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class WorkoutFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let { }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_workout, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @return A new instance of fragment WorkoutFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance() =
+            WorkoutFragment().apply {
+                arguments = Bundle().apply {  }
+            }
+    }
+}
